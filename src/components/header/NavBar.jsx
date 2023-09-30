9f9b56fe10f50b6ac74b42cfdbc529863030be44
Kimaru <ropdeno@gmail.com>
2023-09-30 23:56:36 +0300

    Added Navigation content

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -4,7 +4,7 @@ import "../../styles/NavBar.css";
 
 function NavBar() {
 
-    const navNames = ["Routes", "context API", "HOC","Render props", "HTTP", "Internationalization", "Form validation", "Animation"];
+    const navNames = ["Routes", "Context API", "HOC","Render props", "HTTP", "Internationalization", "Form validation", "Animation", "Axios"];
     const buttonStyle = {
         flex: `1 1 calc(100% / ${navNames.length})`
     };
